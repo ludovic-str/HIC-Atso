4d4eb6c213974c9bb83a10eeffb3be029e2013fa
Mathieu Brujan <ken@localhost.localdomain>
2022-11-26 18:11:30 +0100

    feat: itinerary

--- front/src/components/map/index.tsx
+++ front/src/components/map/index.tsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import fetchWorksites from "../../api/fetchWorksites";
 import { WorksiteInfos } from "../../types";
 import "./index.css";
+import ItineraryForm from "../itenaryForm";
+import {Control} from "leaflet";
 
 const Map = () => {
   const [worksitesInfos, setWorksitesInfos] = useState<WorksiteInfos[]>([]);
+  const [control, setControl] = useState<Control | null>(null)
 
   useEffect(() => {
     fetchWorksites().then((infos) => {
@@ -19,21 +22,22 @@ const Map = () => {
   console.log(worksitesInfos);
 
   return (
-    <MapContainer
-      center={[48.86195408599429, 2.3470007271109825]}
-      zoom={15}
-      scrollWheelZoom={true}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[48.86195408599429, 2.3470007271109825]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+  <>
+    <ItineraryForm control={control} setControl={setControl}/>
+    <div className="container-map">
+      <MapContainer
+        center={[48.86195408599429, 2.3470007271109825]}
+        zoom={15}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+
+  </>
   );
 };
 
